3bc0eebaa3574be3e00e45d4dc93e20bf5232916
haruka.saiga <ec2-user@ip-172-31-26-50.us-east-2.compute.internal>
2018-07-04 03:53:27 +0000

    branch1

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -5,14 +5,14 @@
         <div class="container">
             <div class="navbar-header">
                
-                <a class="navbar-brand" href="/">Cocktail Maker</a>
+                <a class="navbar-brand" href="/">Cocktail Maker Branch1</a>
             </div>
              
            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                 
                
-                    <a class="navbar-brand" href="/">Register Change</a>
+                    <a class="navbar-brand" href="/">Register</a>
                    
                 </ul>
             </div>    
